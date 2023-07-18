b0e272abbb02e9fc2d03ea53ca9bf0bf1be45920
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-07-18 17:56:26 +0200

    create components

--- src/index.js
+++ src/index.js
@@ -49,11 +49,36 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div>
+      <h1>Fast React Pizza Co.</h1>
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>
   );
 }
 
